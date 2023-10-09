94aae94351d58014a74ecf6436444be9aa7232e4
Anurag Singh Rajput <145913972+Daya1511@users.noreply.github.com>
2023-10-09 17:58:52 +0530

    Create Anurag(maximum subarray).java

---
+++ Anurag(maximum subarray).java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int cursum=0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            cursum=cursum+nums[i];
+            if(cursum>maxsum)
+            {
+                maxsum=cursum;
+            }
+            if(cursum<0)
+            {
+                cursum=0;
+            }
+          
+        }
+        return maxsum;
+        
+    }
+}
